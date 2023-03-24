8808a6e6d5fcdcf83e6965284bd33be04a295714
oliverspeer <oliver.speer@startmail.com>
2023-03-24 15:11:19 +0100

    included age. started to count orders

---
+++ countUniquepermonth.R
@@ -0,0 +1,17 @@
+library(lubridate)
+library(dplyr)
+
+# create a sample data frame
+df <- data.frame(
+  id = c(1, 2, 3, 4, 5),
+  date = c("2022-01-05", "2022-02-07", "2022-01-12", "2022-03-21", "2022-02-15"),
+  variable = c("A", "B", "C", "A", "B")
+)
+
+# convert date column to month-year format
+df$month_year <- format(ymd(df$date), "%Y-%m")
+
+# count unique variables per month
+df %>%
+  group_by(month_year) %>%
+  summarize(count = n_distinct(variable))
