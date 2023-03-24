635383593583cae072e356186c22f343b745393a
oliverspeer <oliver.speer@startmail.com>
2023-03-24 08:26:15 +0100

    updated README

---
+++ CountAnaElektrolyAU.R
@@ -0,0 +1,44 @@
+library(readxl)
+global.data <- read_excel("globaldata.xlsx", col_types = 
+                            c("text", "skip", "skip", "skip", "date", "text", "text", 
+                              "numeric", "numeric","numeric","numeric","numeric","numeric"))
+# elektrolyte <- read_xlsx("C:\\R_local\\labStat\\MethodenStatistik_elektrol.xlsx")
+
+
+global.data$Datum <- ymd(substr(global.data$Tagesnummer, 1, 10))
+
+Analysen <- global.data |> pivot_longer(cols = Calcium:Phosphat, names_to = "Parameter", values_to = "Werte")
+
+
+
+Analysen.count <- global.data |> group_by(Datum) |>
+  summarise(
+    Natrium.c = sum(!is.na(Natrium)),
+    Kalium.c = sum(!is.na(Kalium)),
+    Calcium.c = sum(!is.na(Calcium)),
+    Chlorid.c = sum(!is.na(Chlorid)),
+    Aufträge = sum(!is.na(Tagesnummer))
+  )
+
+
+Analysen.sort <- global.data.sort |> pivot_longer(cols = Natrium.c:Calprotectin.c, names_to = "Parameter", values_to = "Anzahl")
+
+# keine sehr einprägsame Darstellung, informativer ist das Histogramm unten
+ggplot(data = Analysen.sort) + 
+  geom_point(aes(x = Datum, y = Anzahl)) +
+  facet_wrap(~ Parameter, scales = "free_y")
+
+#ggplot vom data.frame "Analysen". Kein "summarise" nötig?
+
+ggplot(data = Analysen, aes(x = Datum, fill = Geschl.)) + 
+  geom_histogram(binwidth = 0.5, na.rm = TRUE) +
+  facet_wrap(~ Parameter) 
+
+
+ggplot(Analysen, aes(x = Datum, fill = Auftragg.)) + #wegen sehr vieler Auftragg.
+  geom_bar() +                                      # nicht sehr sinnvoll
+  facet_wrap(~ Parameter)
+
+ggplot(Analysen, aes(x=Datum)) +
+  geom_histogram(Tagesnummer)
+  
--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 labStat README
 next steps:
--) readxl, DONE
--) eliminate duplicate rows? DONE
--) ad tax data
--) fuse row 2 with row 1 DONE
--) pivot long with title split
+-) readxl, DONE 20.3.23
+-) eliminate duplicate rows? DONE 23.3.23
+-) ad tax data DONE 24.3.23
+-) fuse row 2 with row 1 DONE 23.3.23
+-) pivot long with title split 23.3.23
+
+sum unique orders
+sum tax points
+over quarters
+% in/decrease
+add units
+go for refference intervals
