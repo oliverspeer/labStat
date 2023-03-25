e31f837450ebb123b3bfcfc6a09531ca6c66a916
oliverspeer <oliver.speer@oliver.speer>
2023-03-25 19:47:58 +0100

    filtered, summarized date, with pivot_wider into a new data.frame inserted 2 flex tabel for AU, inserted 2 flex tabels for DXI,

--- CountAnaElektrolyAU.R
+++ CountAnaElektrolyAU.R
@@ -1,29 +1,7 @@
 library(readxl)
-global.data <- read_excel("globaldata.xlsx", col_types = 
-                            c("text", "skip", "skip", "skip", "date", "text", "text", 
-                              "numeric", "numeric","numeric","numeric","numeric","numeric"))
-# elektrolyte <- read_xlsx("C:\\R_local\\labStat\\MethodenStatistik_elektrol.xlsx")
 
 
-global.data$Datum[2] <- ymd(substr(global.data$Tagesnummer, 1, 10))
-
-Analysen <- global.data |> pivot_longer(
-  cols = !(a_Tagesnummer:Datum), 
-  names_to = c("Bezeichnung","Methode"),
-  names_sep = "_",
-  values_to = "Werte")
-
-analy.tarif <-
-  left_join(
-    DxI1.qm.data,
-    DxI2.qm.data,
-    by = c("Datum", "Level", "Parameter", "Zielwert", "SD", "Proben-ID"),
-    multiple = "any",
-    na_matches = "never"
-  )
-
-
-
+#wie fürs QC gelernt:
 Analysen.count <- global.data |> group_by(Datum) |>
   summarise(
     Natrium.c = sum(!is.na(Natrium)),
@@ -32,6 +10,34 @@ Analysen.count <- global.data |> group_by(Datum) |>
     Chlorid.c = sum(!is.na(Chlorid)),
     Aufträge = sum(!is.na(Tagesnummer))
   )
+
+#andere Alternative
+dxi.sum.data <- tidy.dxi.data |> 
+  group_by(Year_Month, Methode) |> 
+  summarize(DxI_TxpUmsatz = sum(!is.na(Taxpkt.)),
+            DxI_Aufträge = n_distinct(a_Tagesnummer),
+            Anz_Patienten = n_distinct(b_Fallnummer),
+            Natrium.c = sum(!is.na(Natrium)),
+            Kalium.c = sum(!is.na(Kalium)),
+            Calcium.c = sum(!is.na(Calcium)),
+            Chlorid.c = sum(!is.na(Chlorid))
+  )
+
+#alternative with dplyr
+dxi.sum.data <- tidy.dxi.data |>
+  summarize(
+    n = n(),
+    DxI_TxpUmsatz = sum(!is.na(Taxpkt.)),
+    DxI_Aufträge = n_distinct(a_Tagesnummer),
+    Anz_Patienten = n_distinct(b_Fallnummer),
+    hsTroponin_I = sum(!is.na("hs Troponin I (DxI)")),
+    PCT = sum(!is.na("Procalcitonin quant. (PCT)")),
+    Cortisol.c = sum(!is.na("Cortisol")),
+    CEA.c = sum(!is.na("CEA")),
+    .by = c(Year_Month, Methode)
+           )
+
+
 
 
 Analysen.sort <- global.data.sort |> pivot_longer(cols = Natrium.c:Calprotectin.c, names_to = "Parameter", values_to = "Anzahl")
