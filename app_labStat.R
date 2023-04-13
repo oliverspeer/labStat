a225260eb865a60ed88c538e2691d67fa479f8e4
oliverspeer <oliver.speer@startmail.com>
2023-04-13 17:34:48 +0200

    added ggplots

--- app_labStat.R
+++ app_labStat.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(tidyverse)
+library(ggplot2)
 
 ui <- fluidPage(
   selectInput("gerät", "Gerät", choices = unique(combined.data.kc$Gerät)),
@@ -8,6 +9,11 @@ ui <- fluidPage(
   fluidRow(
     column(6, tableOutput("quarterly")),
     column(6, tableOutput("yearly"))
+  ),
+  fluidRow(
+    column(4, plotOutput("plot1")),
+    column(4, plotOutput("plot2")),
+    column(4, plotOutput("plot3"))
   )
 )
 
@@ -50,6 +56,33 @@ server <- function(input, output, session) {
         Anz_Aufträge = n_distinct(a_Tagesnummer, na.rm = TRUE),
         Anz_Fälle = n_distinct(b_Fallnummer, na.rm = TRUE)
       )
+  })
+  
+  output$plot1 <- renderPlot({
+    req(input$year, input$methodenbezeichnung)
+    methode() %>% 
+      group_by(Year) %>% 
+      summarize(TxpUmsatz_KC = sum(Taxpkt., na.rm = TRUE)) %>% 
+      ggplot(aes(x = Year, y = TxpUmsatz_KC)) + 
+      geom_line()
+  })
+  
+  output$plot2 <- renderPlot({
+    req(input$year, input$methodenbezeichnung)
+    methode() %>% 
+      group_by(Year) %>% 
+      summarize(Anz_Aufträge = n_distinct(a_Tagesnummer, na.rm = TRUE)) %>% 
+      ggplot(aes(x = Year, y = Anz_Aufträge)) + 
+      geom_line()
+  })
+  
+  output$plot3 <- renderPlot({
+    req(input$year, input$methodenbezeichnung)
+    methode() %>% 
+      group_by(Year) %>% 
+      summarize(Anz_Fälle = n_distinct(b_Fallnummer, na.rm = TRUE)) |> 
+      ggplot(aes(x = Year, y = Anz_Fälle)) +
+      geom_line()
   })
 }
 
