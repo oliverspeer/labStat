2d03323f342e44294db75f141d6b124611fa454c
oliverspeer <oliver.speer@startmail.com>
2023-04-17 15:17:05 +0200

    logo tabPanels theme "reafable"

--- app_labStat.R
+++ app_labStat.R
@@ -1,19 +1,56 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
+library(shinythemes)
+library(flextable)
+library(shinyjs)
 
 ui <- fluidPage(
-  selectInput("gerät", "Gerät", choices = unique(combined.data.kc$Gerät)),
-  selectInput("methodenbezeichnung", "Analysen Methode", choices = NULL),
-  selectInput("year", "Jahr", choices = NULL),
-  fluidRow(
-    column(6, tableOutput("quarterly")),
-    column(6, tableOutput("yearly"))
+  
+  #themeSelector(),
+  list(tags$head(HTML('<href="logo_pos.png",
+                                   type="png" />'))),
+  div(style="padding: 10px 10px; width: '100%'",
+      titlePanel(
+        title="", windowTitle="Statistiken Klinische Chemie"
+      )
   ),
-  fluidRow(
-    column(4, plotOutput("plot1")),
-    column(4, plotOutput("plot2")),
-    column(4, plotOutput("plot3"))
+  navbarPage(
+    title = div(" ", img(src="logo_pos.png", 
+                         height = 28, 
+                         width = 130, 
+                         style = "margin:1px 3px")
+    ), 
+    theme = shinytheme("readable"), 
+    collapsible = TRUE,
+    fluid = TRUE,
+    tabPanel("KPI Klinische Chemie", "Analysen",
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("gerät", "Gerät", choices = unique(combined.data.kc$Gerät)),
+                 selectInput("methodenbezeichnung", "Analysen Methode", choices = NULL),
+                 selectInput("year", "Jahr", choices = NULL)
+               ),
+               mainPanel(
+                 fluidRow(
+                   column(12, tableOutput("quarterly")),
+                   # column(6, tableOutput("yearly"))
+                 ),
+                 fluidRow(
+                   # column(6, tableOutput("quarterly")),
+                   column(12, tableOutput("yearly"))
+                 ),
+                 
+               )
+             )
+    ),
+    tabPanel("Plot", "Verlauf",
+             fluidRow(
+               column(4, plotOutput("plot1")),
+               column(4, plotOutput("plot2")),
+               column(4, plotOutput("plot3"))
+             )
+    )
   )
 )
 
@@ -35,26 +72,28 @@ server <- function(input, output, session) {
     updateSelectInput(inputId = "year", choices = choices)
   })
   
-  output$quarterly <- renderTable({
-    req(input$year, input$methodenbezeichnung)
-    methode() %>% 
-      filter(Year == input$year) %>% 
-      group_by(Quarter) %>% 
-      summarize(
-        TxpUmsatz_KC = sum(Taxpkt., na.rm = TRUE),
-        Anz_Aufträge = n_distinct(a_Tagesnummer, na.rm = TRUE),
-        Anz_Fälle = n_distinct(b_Fallnummer, na.rm = TRUE)
-      )
-  })
+  output$quarterly <- renderTable(hover = TRUE,
+                                  {
+                                    req(input$year, input$methodenbezeichnung)
+                                    methode() %>% 
+                                      filter(Year == input$year) %>% 
+                                      group_by(Quarter) %>% 
+                                      summarize(
+                                        TxpUmsatz_KC = sum(Taxpkt., na.rm = TRUE),
+                                        Anz_Aufträge = n_distinct(a_Tagesnummer, na.rm = TRUE),
+                                        Anz_Fälle = n_distinct(b_Fallnummer, na.rm = TRUE)
+                                      ) 
+                                  })
   
   output$yearly <- renderTable({
     req(input$year, input$methodenbezeichnung)
     methode() %>% 
-      group_by(Year) %>% 
+      #group_by(Year) %>% 
       summarize(
         TxpUmsatz_KC = sum(Taxpkt., na.rm = TRUE),
         Anz_Aufträge = n_distinct(a_Tagesnummer, na.rm = TRUE),
-        Anz_Fälle = n_distinct(b_Fallnummer, na.rm = TRUE)
+        Anz_Fälle = n_distinct(b_Fallnummer, na.rm = TRUE),
+        .by = Year
       )
   })
   
